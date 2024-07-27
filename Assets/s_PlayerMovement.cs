ff4f59c1baddb06f60a74b90d23f2ae953314b28
Gio <44237862+Redifexx@users.noreply.github.com>
2024-07-27 01:20:31 -0400

    jumping

--- Assets/s_PlayerMovement.cs
+++ Assets/s_PlayerMovement.cs
@@ -7,16 +7,29 @@ public class s_PlayerMovement : MonoBehaviour
     [Header("Movement & Animation")]
     public Animator playerAnim;
     public Rigidbody rb;
+    public CharacterController playerController;
     public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed, horizontalInput, verticalInput;
     public float groundDrag;
-    public bool walking;
+    public bool walking = false;
+    public bool idle = true;
+    public bool falling = false;
+    public bool sprinting = false;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2.0f;
     public Transform orientation;
     Vector3 moveDirection;
+    Vector3 playerVelocity;
 
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
-    bool grounded;
+    public bool grounded;
+    public bool inAir;
+
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+    }
 
     void FixedUpdate()
     {
@@ -34,18 +47,16 @@ public class s_PlayerMovement : MonoBehaviour
     void Update()
     {
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
-        AnimationController();
+        inAir = !Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.5f);
+        //grounded = playerController.isGrounded;
+        if (grounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
         MyInput();
-        SpeedControl();
+        MovePlayer();
+        AnimationController();
 
-        if (grounded)
-        {
-            rb.drag = groundDrag;
-        }
-        else
-        {
-            rb.drag = 0;
-        }
     }
 
     private void MyInput()
@@ -56,45 +67,114 @@ public class s_PlayerMovement : MonoBehaviour
 
     private void MovePlayer()
     {
+        //Vector3 move = new Vector3(horizontalInput * orientation.right, 0, verticalInput);
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        rb.AddForce(moveDirection.normalized * w_speed * 1f, ForceMode.Force);
-    }
+        playerController.Move(moveDirection.normalized * w_speed * Time.deltaTime);
 
-    private void SpeedControl()
-    {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        if (flatVel.magnitude > w_speed)
+        if (moveDirection.normalized != Vector3.zero)
         {
-            Vector3 limitedVel = flatVel.normalized * w_speed;
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            gameObject.transform.forward = moveDirection.normalized;
         }
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
+
+        playerVelocity.y += gravity * Time.deltaTime;
+        playerController.Move(playerVelocity * Time.deltaTime);
     }
+
 
     private void AnimationController()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        idle = true;
+        sprinting = false;
+        walking = false;
+        falling = false;
+        if (Input.GetButtonDown("Jump"))
         {
-            playerAnim.SetTrigger("Jog");
-            playerAnim.ResetTrigger("Idle");
+            playerAnim.SetTrigger("Jump");
+        }
+        if (inAir)
+        {
+            playerAnim.SetBool("Falling", true);
+            falling = true;
+        }
+        else
+        {
+            playerAnim.ResetTrigger("Jump");
+            playerAnim.SetBool("Falling", false);
+            falling = false;
+        }
+        if (verticalInput != 0f || horizontalInput != 0f)
+        {
+            idle = false;
             walking = true;
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                walking = false;
+                sprinting = true;
+                w_speed = w_speed + rn_speed;
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                walking = true;
+                sprinting = false;
+                w_speed = olw_speed;
+            }
+
+            if (walking)
+            {
+                playerAnim.SetBool("Jog", true);
+                playerAnim.SetBool("Idle", false);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                playerAnim.SetBool("Sprint", true);
+                playerAnim.SetBool("Jog", false);
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                playerAnim.SetBool("Sprint", false);
+                playerAnim.SetBool("Jog", true);
+            }
         }
-        if (Input.GetKeyUp(KeyCode.W))
+        else
         {
-            playerAnim.ResetTrigger("Jog");
-            playerAnim.SetTrigger("Idle");
-            walking = false;
+            if (!falling || Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                idle = true;
+                walking = false;
+                sprinting = false;
+                w_speed = olw_speed;
+                playerAnim.SetBool("Jog", false);
+                playerAnim.SetBool("Idle", true);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            playerAnim.SetTrigger("JogBack");
-            playerAnim.ResetTrigger("Idle");
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            playerAnim.ResetTrigger("JogBack");
-            playerAnim.SetTrigger("Idle");
-        }
+
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    playerAnim.SetTrigger("Jog");
+        //    playerAnim.ResetTrigger("Idle");
+        //    walking = true;
+        //}
+        //if (Input.GetKeyUp(KeyCode.W))
+        //{
+        //    playerAnim.ResetTrigger("Jog");
+        //    playerAnim.SetTrigger("Idle");
+        //    walking = false;
+        //}
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    playerAnim.SetTrigger("JogBack");
+        //    playerAnim.ResetTrigger("Idle");
+        //}
+        //if (Input.GetKeyUp(KeyCode.S))
+        //{
+        //    playerAnim.ResetTrigger("JogBack");
+        //    playerAnim.SetTrigger("Idle");
+        //}
         //if (Input.GetKey(KeyCode.A))
         //{
         //    playerTrans.Rotate(0, -ro_speed * Time.deltaTime, 0);
@@ -104,20 +184,20 @@ public class s_PlayerMovement : MonoBehaviour
         //    playerTrans.Rotate(0, ro_speed * Time.deltaTime, 0);
         //}
 
-        if (walking == true)
-        {
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                w_speed = w_speed + rn_speed;
-                playerAnim.SetTrigger("Sprint");
-                playerAnim.ResetTrigger("Jog");
-            }
-            if (Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                w_speed = olw_speed;
-                playerAnim.ResetTrigger("Sprint");
-                playerAnim.SetTrigger("Jog");
-            }
-        }
+        //if (walking == true)
+        //{
+        //    if (Input.GetKeyDown(KeyCode.LeftShift))
+        //    {
+        //        w_speed = w_speed + rn_speed;
+        //        playerAnim.SetTrigger("Sprint");
+        //        playerAnim.ResetTrigger("Jog");
+        //    }
+        //    if (Input.GetKeyUp(KeyCode.LeftShift))
+        //    {
+        //        w_speed = olw_speed;
+        //        playerAnim.ResetTrigger("Sprint");
+        //        playerAnim.SetTrigger("Jog");
+        //    }
+        //}
     }
 }
